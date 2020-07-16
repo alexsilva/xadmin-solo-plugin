fdbf21f9121468bda58d0df522c4ef6605175624
alex <geniofuturo@gmail.com>
2020-07-16 13:16:39 -0300

    Setup + initial package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xadmin-solo-plugin',
+    version='1.0.0',
+    packages=['xplugin_solo'],
+    url='https://github.com/alexsilva/xadmin-solo-plugin',
+    license='MIT',
+    author='alex',
+    author_email='alex@fabricadigital.com.br',
+    description='Site configuration with xadmin'
+)
