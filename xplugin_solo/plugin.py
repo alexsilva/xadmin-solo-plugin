6b3b9b9a7d0e34f381838b4e5c40e47c3dd053e8
alex <geniofuturo@gmail.com>
2020-07-16 17:45:26 -0300

    Changing context permissions.

--- xplugin_solo/plugin.py
+++ xplugin_solo/plugin.py
@@ -11,3 +11,18 @@ class SoloPlugin(BaseAdminPlugin):
         model = getattr(self.admin_view, 'model', None)
         return bool(inspect.isclass(model) and
                     issubclass(model, SingletonModel))
+
+    def _get_config(self):
+        model = self.admin_view.model
+        try:
+            config = model.objects.get()
+        except model.DoesNotExist:
+            config = None
+        return config
+
+    def get_context(self, context):
+        context['has_add_permission'] = self._get_config() is None
+        context['has_delete_permission'] = False
+        context['show_save_and_add_another'] = False
+        context['show_delete_link'] = False
+        return context
