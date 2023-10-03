63d24dd09808dce38226b796a6d3ebeeed067ce1
alexsilva <geniofuturo@gmail.com>
2023-10-03 17:36:54 -0300

    Making the url configuration work from the CommAdminView.

--- xplugin_solo/plugin.py
+++ xplugin_solo/plugin.py
@@ -10,40 +10,42 @@ from xadmin.views import BaseAdminPlugin
 def get_model_url(self, model, name, *args, **kwargs):
     """Redirects to the change or add screens based on the configuration
     (the changelist will no longer be used as it only has one record)"""
-    if issubclass(model, self.model) and name == 'changelist':
-        prefix = self.opts.label_lower.replace('.', "_")
-        solo_config = getattr(self, f"__{prefix}_solo", None)
+    if issubclass(model, self.get_model_url.plugin.singleton_model) and name == 'changelist':
+        solo_config = self.get_model_url.plugin.get_solo_config(model)
         return (self.get_model_url(model, 'change', solo_config.pk, *args, **kwargs)
                 if solo_config else self.get_model_url(model, 'add', *args, **kwargs))
     else:
         return type(self).get_model_url(self, model, name, *args, **kwargs)
 
 
-class SoloPlugin(BaseAdminPlugin):
-    """Solo config plugin"""
-
-    def init_request(self, *args, **kwargs):
-        model = getattr(self.admin_view, 'model', None)
-        return bool(inspect.isclass(model) and
-                    issubclass(model, SingletonModel))
+class SoloUrlPlugin(BaseAdminPlugin):
+    singleton_model = SingletonModel
 
     def setup(self, *args, **kwargs):
-        prefix = self.opts.label_lower.replace('.', "_")
-        setattr(self.admin_view, f"__{prefix}_solo", self._solo_config)
         plugin = getattr(self.admin_view.get_model_url, 'plugin', None)
-        if plugin is None or not isinstance(plugin, self) and hasattr(self.admin_view, "get_model_url"):
+        if plugin is None or not isinstance(plugin, type(self)) and hasattr(self.admin_view, "get_model_url"):
             self.admin_view.get_model_url = partial(get_model_url, self.admin_view)
             self.admin_view.get_model_url.plugin = self
 
-    @cached_property
-    def _solo_config(self):
+    def get_solo_config(self, model: SingletonModel):
         """Returns the first configuration created"""
-        model = self.admin_view.model
         try:
             config = model.objects.get(pk=model.singleton_instance_id)
         except model.DoesNotExist:
             config = None
         return config
+
+
+class SoloPlugin(SoloUrlPlugin):
+    """Solo config plugin"""
+
+    def init_request(self, *args, **kwargs):
+        model = getattr(self.admin_view, 'model', None)
+        return bool(inspect.isclass(model) and issubclass(model, SingletonModel))
+
+    @cached_property
+    def _solo_config(self):
+        return self.get_solo_config(self.model)
 
     def get_context(self, context):
         context['has_add_permission'] = self._solo_config is None
--- xplugin_solo/sites.py
+++ xplugin_solo/sites.py
@@ -1,5 +1,5 @@
 from xadmin.sites import site as xadmin_site
-from xadmin.views import UpdateAdminView, ListAdminView, CreateAdminView
+from xadmin.views import CommAdminView, UpdateAdminView, ListAdminView, CreateAdminView
 
 
 def register(site=None):
@@ -7,8 +7,9 @@ def register(site=None):
     if site is None:
         site = xadmin_site
 
-    from xplugin_solo.plugin import SoloPlugin
+    from xplugin_solo.plugin import SoloUrlPlugin, SoloPlugin
 
+    site.register_plugin(SoloUrlPlugin, CommAdminView)
     site.register_plugin(SoloPlugin, CreateAdminView)
     site.register_plugin(SoloPlugin, UpdateAdminView)
     site.register_plugin(SoloPlugin, ListAdminView)
